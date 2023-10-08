b16b2ec42af2e235c2cbd8c5b9a72ffed1ef56da
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-10-08 08:05:23 +0530

    packages installed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+import express from "express";
+import { google } from "googleapis";
+
+const app = express();
+
+const PORT = process.env.NODE_ENV || 8000;
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+const scopes = ["https://www.googleapis.com/auth/calendar"];
+
+app.get("/google", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+
+  res.redirect(url);
+});
+
+app.get("/google/redirect", (req, res) => {
+  res.send("its working");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port${PORT}`);
+});
